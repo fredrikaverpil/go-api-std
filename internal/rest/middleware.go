eb06ebd1a9a7b1db2fb048f4559a531c04a9a2e5
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-10-19 17:26:45 +0200

    feat: use middleware for every request

--- internal/rest/middleware.go
+++ internal/rest/middleware.go
@@ -5,10 +5,35 @@ import (
 	"net/http"
 )
 
+// MiddlewareServeMux wraps http.ServeMux and applies middleware to all requests
+type MiddlewareServeMux struct {
+	*http.ServeMux
+	middlewares []func(http.HandlerFunc) http.HandlerFunc
+}
+
+func NewMiddlewareServeMux() *MiddlewareServeMux {
+	return &MiddlewareServeMux{ServeMux: http.NewServeMux()}
+}
+
+func (m *MiddlewareServeMux) Use(middleware func(http.HandlerFunc) http.HandlerFunc) {
+	m.middlewares = append(m.middlewares, middleware)
+}
+
+func (m *MiddlewareServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var handler http.HandlerFunc = m.ServeMux.ServeHTTP
+
+	// Apply middlewares in reverse order
+	for i := len(m.middlewares) - 1; i >= 0; i-- {
+		handler = m.middlewares[i](handler)
+	}
+
+	handler.ServeHTTP(w, r)
+}
+
+// LogMiddleware is now a middleware function
 func LogMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.Method, r.URL)
-
 		next.ServeHTTP(w, r)
 	}
 }
--- internal/rest/server.go
+++ internal/rest/server.go
@@ -9,7 +9,7 @@ import (
 
 type Server struct {
 	listenAddr  string
-	router      *http.ServeMux
+	router      *MiddlewareServeMux
 	userService user.UserService
 }
 
@@ -17,12 +17,14 @@ func NewServer(listenAddr string, userService user.UserService) *Server {
 	server := Server{
 		listenAddr:  listenAddr,
 		userService: userService,
-		router:      http.NewServeMux(),
+		router:      NewMiddlewareServeMux(),
 	}
 
-	// middleware for all requests
-	// FIXME: this only uses the middleware for the default handler
-	server.router.HandleFunc("/", LogMiddleware(server.DefaultHandler))
+	// Add middleware for every request
+	server.router.Use(LogMiddleware)
+
+	// Default handler
+	server.router.HandleFunc("/", server.DefaultHandler)
 
 	// serve all static files at /static from the ./static folder
 	staticFolderHandler := http.FileServer(http.Dir("./static"))
